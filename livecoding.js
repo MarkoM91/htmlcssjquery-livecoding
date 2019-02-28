98e8b4b9c7dbf2b91aac3b2d7b2e6193adfa91c4
MarkoM91 <markomedic91@gmail.com>
2019-02-28 15:14:24 +0100

    commit1

---
+++ livecoding.js
@@ -0,0 +1,17 @@
+var circle = $('div#circle');
+
+circle.hover(function() {
+
+  circle.addClass(function() {
+
+    circle.html("LIVE CODING");
+  });
+}, function() {
+  circle.removeClass(function() {
+
+    circle.html("LIVE CODING");
+  })
+}
+
+
+});
